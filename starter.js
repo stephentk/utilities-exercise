90e9a66244d4a9f159d02f8d73e1f9edba07af82
Raphael Etim <raphaelumoh@gmail.com>
2022-01-18 09:16:00 +0100

    update starter.js

--- starter.js
+++ starter.js
@@ -1,1 +1,31 @@
+// find the largest number in an array
+const largeNumber = (arr) => {
+  let largest = 0;
+  arr.forEach((x) => {
+    if(x > largest){
+      largest = x;
+    }
+  })
+  return largest;
 
+}
+
+// remove the 3rd item from array
+const cut3 = (arr) => {
+  arr.splice(2, 1);
+  return arr
+}
+
+// add numbers in an array
+const sum = (arr) => {
+  const total = 0;
+  arr.forEach((x) => {
+    total += x;
+  })
+  return total
+}
+
+// concatenate two arrays
+const concat = (arr1, arr2) => {
+  return [...arr1, ...arr2]
+}
